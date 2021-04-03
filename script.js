ea926a77c8e01818240964ca01c26f7ccd78fa6b
AmbrosovAlexandr <81846470+AmbrosovAlexandr@users.noreply.github.com>
2021-04-03 11:04:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+button.onclick = function () {
+  if (button.style.backgroundColor == 'blue') {
+    button.style.backgroundColor = 'white';
+    button.style.color = 'black';
+  } else {
+	button.style.backgroundColor = 'blue';
+    button.style.color = 'white';
+  }
+}
